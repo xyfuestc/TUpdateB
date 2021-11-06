2b91f3e6fbf1d255f3555694d184de9edf966448
Sean <xyf_uestc@163.com>
2021-11-06 21:07:19 +0800

    正确删除cmdList

--- schedule/tupdate.go
+++ schedule/tupdate.go
@@ -46,21 +46,18 @@ func (M *CMDWaitingList) updateRunnableCMDs(blockID int)  {
 func (M *CMDWaitingList) popRunnableCMDs() []*config.CMD  {
 	M.Lock()
 	cmds := make([]*config.CMD, 0, len(M.Queue))
-	//indexes := make([]int, 0, len(M.Queue))
-	//找出可执行命令
 	for _, v := range M.Queue {
 		if len(v.Helpers) == 0 {
 			cmds = append(cmds, v)
-			//indexes = append(indexes, cmd)
 		}
 	}
 
 	//删除
-	//sort.Ints(indexes)
-	for i, cmd := range M.Queue {
-		if len(cmd.Helpers) == 0 {
-			M.Queue[len(M.Queue)-1], M.Queue[i] = M.Queue[i], M.Queue[len(M.Queue)-1]
-			M.Queue = M.Queue[:len(M.Queue)-1]
+	for i:= 0; i < len(M.Queue); {
+		if len(M.Queue[i].Helpers) == 0 {
+			M.Queue = append(M.Queue[:i], M.Queue[i+1:]...)
+		} else {
+			i++
 		}
 	}
 
