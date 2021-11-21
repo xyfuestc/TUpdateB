56f0139aaa5bf84606cad45a66677e5ad933fbfc
Sean <xyf_uestc@163.com>
2021-11-21 15:30:14 +0800

    修改base，handleOneTD改为单线程

--- client/client.go
+++ client/client.go
@@ -19,7 +19,7 @@ var numOfUpdatedBlocks = 0
 var numOfUserRequests = 0
 const outFilePre = "./request/"
 const outFileSuffix = ".txt"
-const RequestFileName = "./example-traces/hm_1_part_write.csv"
+const RequestFileName = "./example-traces/hm_0_part.csv"
 
 func main() {
 
