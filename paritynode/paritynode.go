7e503707271f4b0a0e209684412c4d3ce0fcc928
Sean <xyf_uestc@163.com>
2022-03-01 22:01:12 +0800

    修复bug：修改发送端间隔为500ms

--- paritynode/paritynode.go
+++ paritynode/paritynode.go
@@ -218,15 +218,16 @@ func msgSorter(receivedAckCh <-chan config.ACK, receivedTDCh <-chan config.TD, r
 			schedule.GetCurPolicy().HandleCMD(&cmd)
 
 		case mtu := <-receivedMultiMTUCh:
-			td := GetTDFromMulticast(mtu)
+			common.PrintMessage(mtu)
+			//td := GetTDFromMulticast(mtu)
 			//模拟接收剩下的切片
-			d := randomDelay(mtu)  //模拟延时（有10%的概率延时）
-			td.Buff = make([]byte, mtu.SendSize)
-			log.Printf("收到sid: %v, blockID: %v, size: %v，模拟延时：%v.", td.SID, td.BlockID, td.SendSize, d)
-			schedule.GetCurPolicy().RecordSIDAndReceiverIP(td.SID, td.FromIP)
-			log.Printf("记录 ackReceiverIP[%v]=%v.", td.SID, td.FromIP)
-			schedule.GetCurPolicy().HandleTD(td)
-			config.TDBufferPool.Put(td)
+			//d := randomDelay(mtu)  //模拟延时（有10%的概率延时）
+			//td.Buff = make([]byte, mtu.SendSize)
+			//log.Printf("收到sid: %v, blockID: %v, size: %v，模拟延时：%v.", td.SID, td.BlockID, td.SendSize, d)
+			//schedule.GetCurPolicy().RecordSIDAndReceiverIP(td.SID, td.FromIP)
+			//log.Printf("记录 ackReceiverIP[%v]=%v.", td.SID, td.FromIP)
+			//schedule.GetCurPolicy().HandleTD(td)
+			//config.TDBufferPool.Put(td)
 
 		}
 	}
