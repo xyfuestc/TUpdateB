3b656ada8e23139a5a8d64208b3ea93ad91359cc
Sean <xyf_uestc@163.com>
2022-03-16 20:28:22 +0800

    修改Multicast的超时时间为500ms

--- datanode/datanode.go
+++ datanode/datanode.go
@@ -15,7 +15,7 @@ import (
 //var connections []net.Conn
 
 var done = make(chan bool)
-var tickerDuration = time.Millisecond * 1000
+var tickerDuration = time.Millisecond * 500
 var ticker = time.NewTicker(tickerDuration)
 
 //func handleCMD(conn net.Conn)  {
